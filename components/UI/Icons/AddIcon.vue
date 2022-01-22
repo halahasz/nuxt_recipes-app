8c9839f08d0ea253bce85aaba18b88db376012c7
Halina Balys <hbalys@gmail.com>
2022-01-22 19:03:39 +0100

    svg icons as separate files

---
+++ components/UI/Icons/AddIcon.vue
@@ -0,0 +1,51 @@
+<template>
+  <svg
+    id="Add_recipe"
+    data-name="Add recipe"
+    xmlns="http://www.w3.org/2000/svg"
+    width="21.463"
+    height="21.463"
+    viewBox="0 0 21.463 21.463"
+  >
+    <g
+      id="Group_21"
+      data-name="Group 21"
+      transform="translate(5.199 5.199)"
+      style="isolation: isolate"
+    >
+      <g id="Group_20" data-name="Group 20" transform="translate(0 0)">
+        <g id="Group_19" data-name="Group 19">
+          <g id="Group_18" data-name="Group 18">
+            <path
+              id="Path_8"
+              data-name="Path 8"
+              d="M37.358,31.944H32.885V27.471a.471.471,0,1,0-.942,0v4.473H27.471a.471.471,0,1,0,0,.942h4.473v4.473a.471.471,0,0,0,.942,0V32.885h4.473a.471.471,0,0,0,0-.942Z"
+              transform="translate(-26.882 -26.882)"
+              fill-rule="evenodd"
+            />
+            <path
+              id="Path_9"
+              data-name="Path 9"
+              d="M32.032,37.565a.589.589,0,0,1-.589-.589V32.621H27.089a.589.589,0,0,1,0-1.177h4.355V27.089a.589.589,0,0,1,1.177,0v4.355h4.355a.589.589,0,1,1,0,1.177H32.621v4.355A.589.589,0,0,1,32.032,37.565Zm-4.944-5.885a.353.353,0,0,0,0,.706h4.591v4.591a.353.353,0,0,0,.706,0V32.385h4.591a.353.353,0,0,0,0-.706H32.385V27.089a.353.353,0,0,0-.706,0v4.591Z"
+              transform="translate(-26.5 -26.5)"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+    <g id="Group_25" data-name="Group 25" style="isolation: isolate">
+      <g id="Group_24" data-name="Group 24">
+        <g id="Group_23" data-name="Group 23">
+          <g id="Group_22" data-name="Group 22">
+            <path
+              id="Path_10"
+              data-name="Path 10"
+              d="M15.147,25.878A10.732,10.732,0,1,1,25.878,15.147,10.744,10.744,0,0,1,15.147,25.878Zm0-20.286A9.555,9.555,0,1,0,24.7,15.147,9.566,9.566,0,0,0,15.147,5.592Z"
+              transform="translate(-4.415 -4.415)"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
