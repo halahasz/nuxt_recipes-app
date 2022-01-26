bc52c4a10d8d9d3c2b13e504d3c760687ff8cec1
Halina Balys <hbalys@gmail.com>
2022-01-26 14:23:47 +0100

    add hover effect for recipe when hovering over heart icon

---
+++ components/UI/Icons/FilledHeart.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    class="recipe-heart"
+    xmlns="http://www.w3.org/2000/svg"
+    width="26.211"
+    height="23.432"
+    viewBox="0 0 26.211 23.432"
+  >
+    <g
+      id="Group_74"
+      data-name="Group 74"
+      transform="translate(-12.005 -16.033)"
+    >
+      <path
+        id="Path_3"
+        data-name="Path 3"
+        d="M18.818,16.033A6.631,6.631,0,0,0,14,18.113a7.3,7.3,0,0,0-.011,10.014L24.606,39.262a.719.719,0,0,0,1,0q5.313-5.56,10.617-11.124a7.294,7.294,0,0,0,0-10.014,6.627,6.627,0,0,0-9.647,0l-1.466,1.52-1.466-1.531a6.548,6.548,0,0,0-4.829-2.08Z"
+        transform="translate(0 0)"
+        fill="#f86600"
+      />
+    </g>
+  </svg>
+</template>
--- components/UI/Recipe.vue
+++ components/UI/Recipe.vue
@@ -12,54 +12,31 @@
         <h2>{{ recipe.title }}</h2>
       </article>
     </nuxt-link>
-    <svg
-      class="recipe-heart"
+    <FilledHeart
       v-if="recipe.liked"
-      @click.stop="onLikeClick"
-      xmlns="http://www.w3.org/2000/svg"
-      width="26.211"
-      height="23.432"
-      viewBox="0 0 26.211 23.432"
-    >
-      <g
-        id="Group_74"
-        data-name="Group 74"
-        transform="translate(-12.005 -16.033)"
-      >
-        <path
-          id="Path_3"
-          data-name="Path 3"
-          d="M18.818,16.033A6.631,6.631,0,0,0,14,18.113a7.3,7.3,0,0,0-.011,10.014L24.606,39.262a.719.719,0,0,0,1,0q5.313-5.56,10.617-11.124a7.294,7.294,0,0,0,0-10.014,6.627,6.627,0,0,0-9.647,0l-1.466,1.52-1.466-1.531a6.548,6.548,0,0,0-4.829-2.08Z"
-          transform="translate(0 0)"
-          fill="#f86600"
-        />
-      </g>
-    </svg>
-    <svg
-      class="recipe-heart"
+      @click.native.stop="onLikeClick"
+      @mouseover.native="onHover"
+      @mouseleave.native="onMouseLeave"
+    />
+    <EmptyHeart
       v-else
-      @click.stop="onLikeClick"
-      xmlns="http://www.w3.org/2000/svg"
-      width="26.211"
-      height="23.432"
-      viewBox="0 0 26.211 23.432"
-    >
-      <path
-        id="Path_3"
-        data-name="Path 3"
-        d="M18.818,16.033A6.631,6.631,0,0,0,14,18.113a7.3,7.3,0,0,0-.011,10.014L24.606,39.262a.719.719,0,0,0,1,0q5.313-5.56,10.617-11.124a7.294,7.294,0,0,0,0-10.014,6.627,6.627,0,0,0-9.647,0l-1.466,1.52-1.466-1.531a6.548,6.548,0,0,0-4.829-2.08Zm0,1.358a5.3,5.3,0,0,1,3.837,1.682l1.962,2.059a.718.718,0,0,0,1,0l1.951-2.048a5.206,5.206,0,0,1,7.664,0,5.914,5.914,0,0,1,0,8.106q-5.06,5.295-10.122,10.6L14.991,27.178a5.916,5.916,0,0,1,0-8.106A5.274,5.274,0,0,1,18.818,17.391Z"
-        transform="translate(-12.005 -16.033)"
-        fill="#fff"
-      />
-    </svg>
+      @click.native.stop="onLikeClick"
+      @mouseover.native="onHover"
+      @mouseleave.native="onMouseLeave"
+    />
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from "vuex";
+import FilledHeart from "@/components/UI/Icons/FilledHeart";
+import EmptyHeart from "@/components/UI/Icons/EmptyHeart";
 
 export default {
-  name: "Recipe",
+  components: {
+    FilledHeart,
+    EmptyHeart
+  },
   props: {
     recipe: {
       type: Object,
@@ -145,6 +122,14 @@ export default {
           }`
         });
       }
+    },
+    onHover() {
+      this.$vnode.elm.querySelector(".recipe-preview").classList.add("hovered");
+    },
+    onMouseLeave() {
+      this.$vnode.elm
+        .querySelector(".recipe-preview")
+        .classList.remove("hovered");
     }
   }
 };
@@ -156,6 +141,7 @@ export default {
   transition: $transition;
   flex-basis: 33.3333%;
   padding: 0 15px;
+  // TODO: media queries
   @media (max-width: 1300px) {
     width: 50%;
   }
@@ -180,15 +166,6 @@ export default {
   right: 40px;
   z-index: 10;
 }
-// .black-cover {
-//   position: absolute;
-//   content: "";
-//   width: 100%;
-//   height: 100%;
-//   top: 0;
-//   background: linear-gradient(rgba(0, 0, 0, 0.4) 40%, rgba(0, 0, 0, 0.8));
-//   transition: all 1s cubic-bezier(0.5, 0, 0.5, 1);
-// }
 .recipe-preview {
   filter: brightness(1.3);
   width: 100%;
@@ -205,9 +182,7 @@ export default {
   overflow: hidden;
   position: relative;
   border: 0;
-  transition: color 0.25s cubic-bezier(0.5, 0, 0.5, 1),
-    transform 0.25s cubic-bezier(0.5, 0, 0.5, 1),
-    background 0.25s cubic-bezier(0.5, 0, 0.5, 1);
+  transition: $transition;
   @media (max-width: 480px) {
     height: 400px;
   }
@@ -218,69 +193,17 @@ export default {
     height: 100%;
     top: 0;
     background: linear-gradient(rgba(0, 0, 0, 0.4) 40%, rgba(0, 0, 0, 0.8));
-    transition: all 1s cubic-bezier(0.5, 0, 0.5, 1);
+    transition: $transition;
   }
-  // &:before,
-  // &.hovered:before,
-  // &:after,
-  // &.hovered:after {
-  //   content: "";
-  //   box-sizing: inherit;
-  //   position: absolute;
-  //   z-index: 1;
-  //   border-radius: 30px;
-  //   border: 2px solid transparent;
-  //   width: 0;
-  //   height: 0;
-  // }
-  // &:before,
-  // &.hovered:before {
-  //   top: 0;
-  //   left: 0px;
-  //   z-index: 3;
-  // }
-  // &:after,
-  // &.hovered:after {
-  //   bottom: 0;
-  //   right: 0;
-  //   z-index: 2;
-  // }
-  // &:hover,
-  // &.hovered {
-  //   color: $primary;
-  // }
   &:hover,
   &.hovered {
     transform: scale(1.05);
-    filter: brightness(0.9);
+    filter: brightness(1.5);
+    transition: $transition;
     &:before {
-      background: linear-gradient(
-        rgba($primary, 0.3) 40%,
-        rgba(211, 116, 7, 0.6)
-      );
+      background-color: rgba($primary, 0.2);
     }
   }
-  // &:hover::before,
-  // &.hovered:before,
-  // &:hover::after,
-  // &.hovered:after {
-  //   width: 100%;
-  //   height: 100%;
-  //   border-radius: 30px;
-  // }
-  // &:hover::before,
-  // &.hovered:before {
-  //   border-top-color: $primary;
-  //   border-right-color: $primary;
-  //   transition: width 0.15s ease-out, height 0.15s ease-out 0.15s;
-  // }
-  // &:hover::after,
-  // &.hovered:after {
-  //   border-bottom-color: $primary;
-  //   border-left-color: $primary;
-  //   transition: border-color 0s ease-out 0.3s, width 0.15s ease-out 0.3s,
-  //     height 0.15s ease-out 0.45s;
-  // }
   h2 {
     width: 100%;
     margin: 0 auto;
